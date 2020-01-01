a6aac444b72a22768aabe8196a7a9adeb5a97235
newaj <linuxunixstorm@gmail.com>
2020-01-01 23:41:05 +0600

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,15 @@
 import 'package:FirebaseUserSignIn/blocs/authBloc/auth_bloc.dart';
 import 'package:FirebaseUserSignIn/blocs/authBloc/auth_state.dart';
-import 'package:FirebaseUserSignIn/repositories/user_repository.dart';
 import 'package:FirebaseUserSignIn/ui/pages/home_page.dart';
 import 'package:FirebaseUserSignIn/ui/pages/login_page.dart';
 import 'package:FirebaseUserSignIn/ui/pages/splash_page.dart';
 import 'package:flutter/material.dart';
-import 'package:meta/meta.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'blocs/authBloc/auth_event.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
-  final UserRepository userRepository = UserRepository();
 
   @override
   Widget build(BuildContext context) {
